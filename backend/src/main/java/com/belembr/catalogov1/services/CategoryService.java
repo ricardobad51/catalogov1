469250a494da0f5973877840519b23ce36625348
ricardobad51 <rbbelem@gmail.com>
2023-01-25 15:05:55 -0300

    Put Category

--- backend/src/main/java/com/belembr/catalogov1/services/CategoryService.java
+++ backend/src/main/java/com/belembr/catalogov1/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.belembr.catalogov1.dto.CategoryDTO;
 import com.belembr.catalogov1.entities.Category;
 import com.belembr.catalogov1.repositories.CategoryRepository;
-import com.belembr.catalogov1.services.exceptions.EntityNotFoundException;
+import com.belembr.catalogov1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -29,7 +31,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = categoryRepository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
 		
 		return new CategoryDTO(entity);
 	}
@@ -42,6 +44,19 @@ public class CategoryService {
 		entity = categoryRepository.save(entity);
 		
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		Category entity = categoryRepository.getReferenceById(id);
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		
+		return new CategoryDTO(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id not Found " + id);
+		}
 	} 
 
 }
